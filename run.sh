a68c1e774d01b7e01e91f6a63f69278dafcb3bce
Rominos111 <romain.theodet@hotmail.fr>
2020-09-07 17:23:32 +0200

    fix: Suppression des .part

--- run.sh
+++ run.sh
@@ -4,14 +4,22 @@ if [ -f ./bootloader.bin ]; then
     mv ./bootloader.bin ./bootloader.bin.bak
 fi
 
+if [ -f ./bootloader.bin.part ]; then
+    rm ./bootloader.bin.part
+fi
+
+if [ -f ./extendedSpace.bin.part ]; then
+    rm ./extendedSpace.bin.part
+fi
+
 echo "Compilation bootloader..."
 nasm bootloader.asm -f bin -o bootloader.bin.part
 
 echo "Compilation secteurs suivants..."
-nasm nextSectors.asm -f bin -o nextSectors.bin.part
+nasm extendedSpace.asm -f bin -o extendedSpace.bin.part
 
 echo "Fusion des fichiers binaires..."
-cat bootloader.bin.part nextSectors.bin.part > bootloader.bin
+cat bootloader.bin.part extendedSpace.bin.part > bootloader.bin
 
 echo "Exécution bootloader..."
 qemu-system-x86_64 -drive format=raw,file=bootloader.bin
