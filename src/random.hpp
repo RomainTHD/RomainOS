968a6ca57d66b355c4dab3dabb84533d4dfdf98c
Romain <romain.theodet@hotmail.fr>
2020-09-22 10:52:30 +0200

    fix: Bon random

--- src/random.hpp
+++ src/random.hpp
@@ -7,16 +7,35 @@
 #include "types.hpp"
 
 /**
- * Précision double
+ * Rand max exclus
  */
-#define RAND_DOUBLE_PRECISION 0x7fffffff
+#define _RAND_MAX 32768
 
 namespace std::random {
     namespace {
         /**
          * Next sous-seed pour le random
          */
-        u32 next = 1;
+        u32 _next = 1;
+
+        /**
+         * Fonction random comme définie dans la bibliothèque C
+         *
+         * @return Nb random entre 0 inclus et 32768 exclus
+         */
+        u32 _randint() {
+            _next = _next * 1103515245 + 12345;
+            return (u32) (_next / (_RAND_MAX * 2)) % _RAND_MAX;
+        }
+    }
+
+    /**
+     * Double aléatoire dans [0;1[
+     *
+     * @return Double
+     */
+    double rand() {
+        return (double) _randint() / (double) _RAND_MAX;
     }
 
     /**
@@ -27,8 +46,19 @@ namespace std::random {
      * @return Random
      */
     u32 randint(u32 max) {
-        next = next * 1103515245 + 12345;
-        return (u32) (next / (max * 2)) % max;
+        return rand() * max;
+    }
+
+    /**
+     * Random integer
+     *
+     * @param min Valeur min
+     * @param max Valeur max
+     *
+     * @return Random
+     */
+    i32 randint(i32 min, i32 max) {
+        return min + rand() * (max - min);
     }
 
     /**
@@ -37,17 +67,10 @@ namespace std::random {
      * @param seed Seed
      */
     void srand(u32 seed) {
-        next = seed;
-    }
-
-    /**
-     * Double aléatoire dans [0;1[
-     *
-     * @return Double
-     */
-    double rand() {
-        return (double) randint(RAND_DOUBLE_PRECISION) / RAND_DOUBLE_PRECISION;
+        _next = seed;
     }
 }
+
+#undef _RAND_MAX
 
 #endif //ROMAINOS_RANDOM_HPP
